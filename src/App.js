368c1bc62854155f4fa1636dd23deba0362cb321
ColeKedz <colerkedz@gmail.com>
2023-12-11 13:18:43 -0500

    Added routes to about me and home

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { Route, Routes, Link } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import About from './components/About';
+import Header from './components/Header.css'
+
+ 
+function App() {
+
+    const linkedinUrl = 'https://www.linkedin.com/in/cole-kedzierski';
+    const githubUrl = 'https://github.com/ColeKedz';
+
+    return (
+
+
+        <div className='App'>
+            <header id="App-header">
+
+                <nav id="navContainer">
+                    <Link className='navItem' to="/reactGames"> Home </Link>
+                    <Link className='navItem' to="/About"> About </Link>
+                    <span ><a className='navItem' href={linkedinUrl} target='_blank' rel='noreferrer' >Linkedin</a></span>
+                    <span><a id='GitHub' className='navItem' href={githubUrl} target='_blank' rel='noreferrer'>GitHub</a></span>
+                    
+                   
+                </nav>
+
+            </header>
+
+            <Routes>
+                <Route path="/reactGames" element={<HomePage />} />
+                <Route path="/About" element={<About /> } />
+            </Routes>
+    
+        </div>
+
+    );
+}
+
+export default App;
---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import './HomePage.css';
+
+function HomePage(){
+  return (
+    <div>
+        <h1 className="App-header">
+                Welcome to Cole's React.js Sandbox!'
+        </h1>
+    </div>
+  );
+}
+
+export default HomePage;
